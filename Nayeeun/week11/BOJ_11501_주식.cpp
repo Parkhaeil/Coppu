a7e6ac059ab3073c33f75c95e631565383cbaa52
Nayeeun5 <dmsdpsk05@gmail.com>
2026-03-29 22:08:33 +0900

    feat: BOJ_11501_주식 구현

---
+++ Nayeeun/week11/BOJ_11501_주식.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+  
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<long long> prices(n);
+        for (int i = 0; i < n; i++) {
+            cin >> prices[i];
+        }
+
+        long long max = 0, profit = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (prices[i] > max)
+                max = prices[i];
+            else
+                profit += max - prices[i];
+        }
+        cout << profit << "\n";
+    }
+}
